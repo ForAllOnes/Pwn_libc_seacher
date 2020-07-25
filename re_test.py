45f76830b62b99edd5a1891a8da9a25c9dcf148c
ForAllOnes <chujiaweipleu@gmail.com>
2020-07-25 20:11:57 +0800

    regular expression is used | try, exception need more work

--- re_test.py
+++ re_test.py
@@ -1,33 +1,38 @@
 #!/usr/bin/env python
 # coding=utf-8
 import re
-
 def get_func(func_name,text):
-    # I don't know how to capture a stringa start with a specific stingb
-    # this part need changes.
-    r = '[\w]*'+func_name+'[\w]* [0-9a-zA-Z]+'
-    r = re.compile(r)
-    ret = r.findall(text)
-    offset = get_specific_func(func_name,ret)
-    while offset == None:
-        for i in ret :
-            print i.split(' ')[0]
-        func_name = raw_input('please select a specific func_name:\n')
-        return get_func(func_name, text)
-    print hex(offset)
-    return offset
-
-def get_specific_func(func_name,ret):
-    for i in ret:
-        i = i.split(' ')
-        name = i[0]
-        offset = i[1]
-        if name == func_name:
-            print name,offset
-            return int(('0x'+offset),16)
-    return None
-
+    offset = get_specific_func(func_name,text)
+    if offset == []:
+        print 'specific find not found.'
+        print 'finding all funcs contains func_name'
+        offset = get_all_func(func_name,text)
+        if len(offset)>=2:
+            for i,j in enumerate(offset):
+                print j.split(' ')[0], i
+            number = input('please select a number:\n')
+            return int ( '0x'+offset[number].split(' ')[1] , 16)
+    elif len(offset) == 1:
+        return int ( '0x'+offset[0].split(' ')[1] , 16)
+    return int('0x'+offset.split(' ')[1], 16)
+def get_all_func(func_name,text):
+    r = '[\w]*' + func_name+'[\w]* [0-9a-zA-Z]+'
+    ret = re.compile(r)
+    return ret.findall(text)
+def get_specific_func(func_name,text):
+    r = r'\n'+func_name+' [0-9a-zA-Z]+'
+    ret = re.compile(r)
+    a = ret.findall(text)
+    if len(a)>=2:
+        if a == [a[0]*len(a)]:
+            return [a[0]]
+    elif len(a)==1:
+        return [a[0].strip()]
+    return []
 if __name__=="__main__":
     with open("./saved.file",'r') as f:
         text = f.read()
-        get_func('exec',text)
+        while(True):
+             func_name = raw_input()
+             offset = get_func(func_name,text)
+             print '\n', hex(offset)
